54ba44c972d508c8965ef5aba12da1a5396898e7
KlausFreitag <2110859005@fh-burgenland.at>
2022-10-27 21:18:44 +0200

    small improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,14 @@ import { HeaderComponent } from './header/header.component';
 import { SensorsDataComponent } from './dashboard/sensors-data/sensors-data.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
+import {MatIconModule,} from '@angular/material/icon';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input'
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import {MatSortModule} from '@angular/material/sort'
 
 @NgModule({
   declarations: [
@@ -36,7 +40,11 @@ import {MatTableModule} from '@angular/material/table';
     MatIconModule,
     MatSliderModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
