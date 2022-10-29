1c6b382c7cdd03456f62ad34d7257dc931e5e579
KlausFreitag <2110859005@fh-burgenland.at>
2022-10-29 21:46:56 +0200

    add: first semi-functioning form

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -2,7 +2,11 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
+import { SensorsDataComponent } from '../dashboard/sensors-data/sensors-data.component';
+import { Sensorendata } from '../Sensorendata';
+import { StoreService } from '../shared/store.service';
 import { DataTableDataSource, DataTableItem } from './data-table-datasource';
+import { BackendService } from '../shared/backend.service';
 
 @Component({
   selector: 'app-data-table',
@@ -13,14 +17,37 @@ export class DataTableComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<DataTableItem>;
-  dataSource: DataTableDataSource;
+
+  listSensorendata!: Sensorendata[]
+  //dataSource: DataTableDataSource;
+  //dataSource: Sensorendata;
+  dataSource: any;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'date', 'sensor', 'temperature'];
 
-  constructor() {
-    this.dataSource = new DataTableDataSource();
+  constructor(private storeservice: StoreService) {
+    //this.dataSource = new DataTableDataSource();
+    //this.dataSource = Sensorendata();
+    this.dataSource = storeservice.sensorenDaten;
   }
+
+  /*
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.backendservice.getSensorenDaten().then(data=>{
+      this.listSensorendata = data;
+      this.
+    }
+       )
+  }
+  */
+
+
+
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
