cb9f57c53e5eb7cd3572075b0ce0025552d0fbd1
KlausFreitag <2110859005@fh-burgenland.at>
2022-11-12 11:30:34 +0100

    new tries for pagination

--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { SensorsDataComponent } from '../dashboard/sensors-data/sensors-data.component';
 import { Sensorendata } from '../Sensorendata';
 import { StoreService } from '../shared/store.service';
@@ -15,14 +15,16 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./data-table.component.scss']
 })
 export class DataTableComponent implements AfterViewInit, OnDestroy {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  //@ViewChild(MatPaginator) paginator!: MatPaginator;
   //@ViewChild(MatSort) sort!: MatSort;
   @ViewChild('empTBSort') empTBSort = new MatSort();
 
   @ViewChild(MatTable) table!: MatTable<DataTableItem>;
+  @ViewChild('paginator') paginator : MatPaginator;
 
   //dataSource: DataTableDataSource;
-  dataSource: any;
+  //dataSource: any;
+  dataSource: MatTableDataSource<any>;
 
   private storeServiceSubscription?: Subscription;
 
@@ -32,10 +34,12 @@ export class DataTableComponent implements AfterViewInit, OnDestroy {
   constructor(private storeservice: StoreService) {
     //this.dataSource = new DataTableDataSource();
     //this.dataSource = Sensorendata();
-    this.dataSource = storeservice.sensorenDaten;
+    //this.dataSource = storeservice.sensorenDaten;
+    this.dataSource = new MatTableDataSource(this.storeservice.sensorenDaten);
   }
 
   ngAfterViewInit(): void {
+    this.dataSource = new MatTableDataSource(this.storeservice.sensorenDaten);
     //this.dataSource.sort = this.sort;
     this.dataSource.sort = this.empTBSort;
     this.dataSource.paginator = this.paginator;
